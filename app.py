0de2e3f959f6c7f44b6adadf45b90c19739ca6e1
Jim Benton <jim@autonomousmachine.com>
2017-12-06 15:21:22 -0600

    Initial import.

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from os import environ, path
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from airtable import Airtable
+
+if environ.get('ENV') != 'production':
+    from dotenv import load_dotenv
+    dotenv_path = path.join(path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+AIRTABLE_APP = environ['AIRTABLE_APP']
+AIRTABLE_API_KEY = environ['AIRTABLE_API_KEY']
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/events')
+def events():
+    events_table = Airtable(AIRTABLE_APP, 'EventsTest', api_key=AIRTABLE_API_KEY)
+
+    events = events_table.search('assignment_status', 'Open Assignment', fields=['id', 'assignment_status', 'name', 'agency_name', 'classification', 'description', 'start_time', 'end_time', 'location_name', 'status'], sort="start_time")
+
+    resp = jsonify(events)
+    return resp
+
+if __name__ == "__main__":
+	app.run()
