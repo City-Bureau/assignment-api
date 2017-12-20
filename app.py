4faad0f5dc52aa73b4623ff238e5cd6e8993c0e8
Jim Benton <jim@autonomousmachine.com>
2017-12-20 00:31:20 -0600

    Support creating assignment applications.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from sys import stdout
 from os import environ, path
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from airtable import Airtable
+from requests import HTTPError
 
 ENV = environ.get('ENV')
 if ENV != 'production':
@@ -14,11 +15,12 @@ if ENV != 'production':
 AIRTABLE_APP = environ['AIRTABLE_APP']
 AIRTABLE_API_KEY = environ['AIRTABLE_API_KEY']
 EVENTS_TABLE = environ['EVENTS_TABLE']
+APPLICATIONS_TABLE = environ['APPLICATIONS_TABLE']
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/events')
+@app.route('/api/events', methods=['GET'])
 def events():
     if ENV != 'production':
         with open('fixtures/events.json', 'r') as json_file:
@@ -29,12 +31,34 @@ def events():
 
     fields = [
       'id', 'assignment_status', 'assignment', 'name', 'agency_name', 'classification',
-      'description', 'start_time', 'end_time', 'location_name', 'status'
+      'description', 'start_time', 'end_time', 'location_name', 'status', 'community_area'
     ]
-    events = events_table.search('assignment_status', 'Open Assignment', fields=fields, sort="start_time")
+    events = events_table.search('assignment_status', 'Open Assignment',
+             fields=fields, sort="start_time")
 
-    resp = jsonify(events)
-    return resp
+    return jsonify(events)
+
+@app.route('/api/applications', methods=['POST'])
+def applications():
+    applications_table = Airtable(AIRTABLE_APP, APPLICATIONS_TABLE, api_key=AIRTABLE_API_KEY)
+
+    j = request.json
+    fields = {
+        'applied_name': j['applied_name'],
+        'email': j['email'],
+        'Event': j['event'],
+        'Agree to Terms': j['agree_to_terms'],
+        'Agree to Rate': j['agree_to_rate'],
+    }
+
+    app.logger.debug('creating assignment application on Airtable: %s' % str(fields))
+    try:
+        applications_table.insert(fields)
+        return jsonify({'message': 'ok'}), 201
+    except HTTPError as error:
+        app.logger.error(error)
+        app.logger.error(error.response.text)
+        return jsonify({'message': 'error'}), 500
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
